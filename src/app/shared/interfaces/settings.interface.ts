e82edb8e8e2db8d823bae36e928da568d3e888d2
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-05-07 09:37:18 +0300

    created SettingsService, use app links from backend

--- src/app/shared/interfaces/settings.interface.ts
+++ src/app/shared/interfaces/settings.interface.ts
@@ -1,10 +1,11 @@
 export interface ISettings {
-    minVoteInterval: number;
-    deletedCredentialsBlockInterval: number;
-    blobUrlPrefix: string;
-    latestAppVersionNumber: number;
-    appStoreUrl: string;
-    googlePlayUrl: string;
-    shareTopicBaseUrl: string;
     apiVersion: string;
+    appStoreUrl: string;
+    blobUrlPrefix: string;
+    defaultAutoPulse: number;
+    deletedCredentialsBlockInterval: number;
+    googlePlayUrl: string;
+    latestAppVersionNumber: number;
+    minVoteInterval: number;
+    shareTopicBaseUrl: string;
 }
