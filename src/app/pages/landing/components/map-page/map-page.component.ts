6beb2a4ce9f5d51672bba8689fc858ac453d0ea7
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-05-08 18:16:30 +0300

    updated the projections switch button

--- src/app/pages/landing/components/map-page/map-page.component.ts
+++ src/app/pages/landing/components/map-page/map-page.component.ts
@@ -1,22 +1,30 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { Projection } from 'mapbox-gl';
-import { IMapMarker } from '@/app/shared/interfaces/map-marker.interface';
+import { Component, inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { Projection } from "mapbox-gl";
+import { IMapMarker } from "@/app/shared/interfaces/map-marker.interface";
 
 @Component({
-    selector: 'app-map-page',
-    templateUrl: './map-page.component.html',
-    styleUrl: './map-page.component.scss',
+    selector: "app-map-page",
+    templateUrl: "./map-page.component.html",
+    styleUrl: "./map-page.component.scss",
 })
 export class MapPageComponent {
     private router: Router = inject(Router);
 
     public projection: Projection["name"] = "mercator";
+    public switchClasses = {};
+
+    ngOnInit() {
+        this.switchClasses = {
+            "map-page__switch": true,
+            "map-page__switch--contrast": this.projection === 'globe'
+        };
+    }
 
     public onMarkerClick(marker: IMapMarker) {
         let newRelativeUrl = this.router.createUrlTree([`topic/${marker.topicId}`]);
-        let baseUrl = window.location.href.replace(this.router.url, '');
-        window.open(baseUrl + newRelativeUrl, '_blank');
+        let baseUrl = window.location.href.replace(this.router.url, "");
+        window.open(baseUrl + newRelativeUrl, "_blank");
     }
 
     public onSwitchChange(checked: boolean) {
