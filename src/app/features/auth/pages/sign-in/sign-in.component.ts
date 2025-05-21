b35e79e872cdbfb9cb80c7780c2cdaae84ff7495
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-05-21 15:39:38 +0300

    finished tel input

--- src/app/features/auth/pages/sign-in/sign-in.component.ts
+++ src/app/features/auth/pages/sign-in/sign-in.component.ts
@@ -1,24 +1,65 @@
-import { Component } from '@angular/core';
-import { SvgIconComponent } from 'angular-svg-icon';
+import {
+    AfterViewInit,
+    Component,
+    inject,
+    OnDestroy,
+    ViewChild,
+} from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { MatInputModule } from "@angular/material/input";
+import { ErrorStateMatcher } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { SvgIconComponent } from "angular-svg-icon";
+import { AppRoutes } from "@/app/shared/enums/app-routes.enum";
 import { PrimaryButtonComponent } from "../../../../shared/components/ui-kit/buttons/primary-button/primary-button.component";
-import { RouterLink } from '@angular/router';
-import { AppRoutes } from '@/app/shared/enums/app-routes.enum';
+import { SignInFormService } from "../../services/sign-in-form.service";
 
 @Component({
-  selector: 'app-sign-in',
-  standalone: true,
-  imports: [RouterLink, SvgIconComponent, PrimaryButtonComponent],
-  templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss'
+    selector: "app-sign-in",
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterLink,
+        MatInputModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        SvgIconComponent,
+        PrimaryButtonComponent,
+    ],
+    providers: [SignInFormService],
+    templateUrl: "./sign-in.component.html",
+    styleUrl: "./sign-in.component.scss",
 })
-export class SignInComponent {
-  private readonly appRotes = AppRoutes;
+export class SignInComponent implements AfterViewInit, OnDestroy {
+    private signInFormService: SignInFormService = inject(SignInFormService);
+    private readonly appRotes = AppRoutes;
 
-  public get termsRoute(): string {
-    return this.appRotes.Community.TERMS;
-  }
+    @ViewChild("telInput") telInput: { nativeElement: HTMLInputElement };
 
-  public get privacyRoute(): string {
-    return this.appRotes.Community.PRIVACY;
-  }
+    public get signInForm(): FormGroup {
+        return this.signInFormService.form;
+    }
+    public get errorStateMatcher(): ErrorStateMatcher {
+        return this.signInFormService.errorStateMatcher;
+    }
+    public get termsRoute(): string {
+        return this.appRotes.Community.TERMS;
+    }
+    public get privacyRoute(): string {
+        return this.appRotes.Community.PRIVACY;
+    }
+
+    ngAfterViewInit(): void {
+        this.signInFormService.onViewInit(this.telInput.nativeElement);
+    }
+
+    ngOnDestroy(): void {
+        this.signInFormService.onDestroy();
+    }
+    
+    public onSubmit() {
+        return this.signInFormService.submit();
+    }
 }
---
+++ src/app/features/auth/services/sign-in-form.service.ts
@@ -0,0 +1,158 @@
+// phone-form.service.ts
+import { inject } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm } from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material/core";
+import intlTelInput, { Iti } from "intl-tel-input";
+import { fr } from "intl-tel-input/i18n";
+import { CountryCode, isValidPhoneNumber, validatePhoneNumberLength } from "libphonenumber-js";
+import { debounceTime, fromEvent, map } from "rxjs";
+
+export class SignInFormService {
+    private readonly formBuilder: FormBuilder = inject(FormBuilder);
+    private iti: Iti;
+    public isValid = true;
+    public countryCodeChanged = false;
+    public countryCode: string = "US";
+    public form: FormGroup;
+    public errorStateMatcher: ErrorStateMatcher;
+
+    constructor() {
+        this.form = this.createForm();
+        this.errorStateMatcher = new CustomErrorStateMatcher(() => this.isValid);
+    }
+
+    private createForm(): FormGroup {
+        return this.formBuilder.group({
+            phone: "",
+        });
+    }
+
+    private validateMaxValueLength(event: KeyboardEvent) {
+        let isValid = false;
+        const numberChars = /[0-9\+\-\ ]/;
+        if (!numberChars.test(event.key)) {
+            isValid = true;
+            return isValid;
+        }
+        const value = this.form.get("phone")?.value;
+        const countryCode = this.iti.getSelectedCountryData().iso2?.toUpperCase();
+        if (!countryCode) return;
+        const validationResult = validatePhoneNumberLength(
+            value + event.key,
+            countryCode as CountryCode,
+        );
+        if (validationResult === "TOO_SHORT" || validationResult === undefined) {
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    private validateMinValueLength(event: KeyboardEvent) {
+        if (event.key !== "Backspace") return true;
+        let isValid = false;
+        const value = this.form.get("phone")?.value;
+        const countryCode = this.iti.getSelectedCountryData().dialCode;
+        if (!countryCode) return;
+        const minValueLength = `+${countryCode}`.length;
+        if (value.length > minValueLength) {
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    private validateChar(event: KeyboardEvent) {
+        let isValid = true;
+        const allowedChars = /[0-9\+\-\ ]/;
+        const allowedCtrlChars = /[axcv]/;
+        const allowedOtherKeys = [
+            "ArrowLeft",
+            "ArrowUp",
+            "ArrowRight",
+            "ArrowDown",
+            "Home",
+            "End",
+            "Insert",
+            "Delete",
+            "Backspace",
+        ];
+
+        if (
+            !allowedChars.test(event.key) &&
+            !(event.ctrlKey && allowedCtrlChars.test(event.key)) &&
+            !allowedOtherKeys.includes(event.key)
+        ) {
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    public onFocus = () => {
+        const value = this.form.get("phone")?.value;
+        if (!value) {
+            const getCode = this.iti.getSelectedCountryData().dialCode;
+            this.form.get("phone")?.setValue("+" + getCode);
+        }
+    };
+
+    public onBlur = () => {
+        if (this.countryCodeChanged) {
+            this.countryCodeChanged = false;
+            return;
+        };
+        this.validateNumber();
+    };
+
+    public onInputKeyDown = (event: KeyboardEvent) => {
+        if (!this.validateChar(event)) {
+            event.preventDefault();
+        } else if (!this.validateMinValueLength(event) || !this.validateMaxValueLength(event)) {
+            event.preventDefault();
+        }
+    };
+
+    public onCountryCodeChange = () => {
+        this.countryCodeChanged = true;
+    };
+
+    public onViewInit = (inputElement: HTMLInputElement) => {
+        this.iti = intlTelInput(inputElement, {
+            nationalMode: false,
+            formatOnDisplay: true,
+            initialCountry: "US",
+            showFlags: true,
+        });
+        fromEvent(inputElement, "blur").pipe(debounceTime(100), map(this.onBlur)).subscribe();
+        fromEvent(inputElement, "focus").pipe(debounceTime(100), map(this.onFocus)).subscribe();
+        fromEvent(inputElement, "countrychange").pipe(map(this.onCountryCodeChange)).subscribe();
+        fromEvent(inputElement, "keydown")
+            .pipe(map((event) => this.onInputKeyDown(event as KeyboardEvent)))
+            .subscribe();
+    };
+
+    public onDestroy = () => {
+        this.iti?.destroy();
+    };
+
+    public validateNumber() {
+        if (!this.iti) return;
+        const value = this.form.get("phone")?.value;
+        const countryCode = this.iti.getSelectedCountryData().iso2?.toUpperCase();
+        if (!countryCode) return;
+
+        const valid = isValidPhoneNumber(value, countryCode as CountryCode);
+        this.isValid = valid;
+    }
+
+    public submit = () => {
+        const phone = this.form.value.phone;
+        console.log("Phone number submitted:", phone);
+    };
+}
+
+class CustomErrorStateMatcher implements ErrorStateMatcher {
+    constructor(private isValidRef: () => boolean) {}
+
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+        return !this.isValidRef();
+    }
+}
