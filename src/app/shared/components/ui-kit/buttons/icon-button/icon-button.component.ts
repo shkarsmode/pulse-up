ac441907afe93695b6198497ab36a4f18b4b7ca0
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-06-11 11:49:17 +0300

    add Add Chip button to the keywords input

--- src/app/shared/components/ui-kit/buttons/icon-button/icon-button.component.ts
+++ src/app/shared/components/ui-kit/buttons/icon-button/icon-button.component.ts
@@ -4,36 +4,16 @@ import { MatButtonModule } from "@angular/material/button";
 @Component({
     selector: "app-icon-button",
     standalone: true,
+    styleUrl: "./icon-button.component.scss",
+    templateUrl: "./icon-button.component.html",
     imports: [MatButtonModule],
-    template: `
-        <button
-            mat-icon-button
-            class="icon-button"
-            [class.icon-button--outline]="variant === 'outline'"
-            [style]="{ width: size + 'px', height: size + 'px' }"
-            (click)="handleClick($event)">
-            <span class="visually-hidden">{{ label }}</span>
-            <ng-content></ng-content>
-        </button>
-    `,
-    styles: `
-      .icon-button {
-        width: 48px;
-        height: 48px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
-      }
-      .icon-button--outline {
-        border: 1px solid var(--light-grey-color);
-      }
-    `,
 })
 export class IconButtonComponent {
     @Input() label: string = "";
     @Input() size: number = 48;
     @Input() variant: "default" | "outline" = "default";
+    @Input() type : "button" | "submit" = "button";
+    @Input() disabled: boolean = false;
 
     @Output() onClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
--- src/app/shared/components/ui-kit/chips-input/chips-input.component.ts
+++ src/app/shared/components/ui-kit/chips-input/chips-input.component.ts
@@ -1,18 +1,15 @@
-import { CommonModule } from '@angular/common';
-import { Component, forwardRef, Input } from '@angular/core';
-import {
-    ControlValueAccessor,
-    FormsModule,
-    NG_VALUE_ACCESSOR
-} from '@angular/forms';
-import { SvgIconComponent } from 'angular-svg-icon';
+import { CommonModule } from "@angular/common";
+import { Component, ElementRef, forwardRef, Input, ViewChild } from "@angular/core";
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { SvgIconComponent } from "angular-svg-icon";
+import { IconButtonComponent } from "../buttons/icon-button/icon-button.component";
 
 @Component({
-    selector: 'app-chips-input',
-    templateUrl: './chips-input.component.html',
-    styleUrl: './chips-input.component.scss',
+    selector: "app-chips-input",
+    templateUrl: "./chips-input.component.html",
+    styleUrl: "./chips-input.component.scss",
     standalone: true,
-    imports: [CommonModule, FormsModule, SvgIconComponent],
+    imports: [CommonModule, FormsModule, SvgIconComponent, IconButtonComponent],
     providers: [
         {
             provide: NG_VALUE_ACCESSOR,
@@ -25,8 +22,14 @@ export class ChipsInputComponent implements ControlValueAccessor {
     @Input() limit: number = 5;
     @Input() hasErrorClass: boolean = false;
 
+    @ViewChild("chipsInput") chipsInput: ElementRef<HTMLInputElement>;
+
     chips: string[] = [];
-    inputValue: string = '';
+    inputValue: string = "";
+
+    get canAddChip(): boolean {
+        return this.chips.length < this.limit && this.inputValue.trim().length > 0;
+    }
 
     public onChange: (value: string[]) => void = () => {};
     public onTouched: () => void = () => {};
@@ -52,35 +55,53 @@ export class ChipsInputComponent implements ControlValueAccessor {
 
         const sanitizeInput = (str: string): string => {
             const regex = /[^a-zA-Z0-9\s]/g;
-            return str.replace(regex, '');
+            return str.replace(regex, "");
         };
 
-        
         setTimeout(() => {
-            input.value = sanitizeInput(this.inputValue);   
+            input.value = sanitizeInput(this.inputValue);
         });
 
         if (
-            (event.key === 'Enter' || event.key === ',' || event.key === ';') &&
-            this.inputValue.trim().length > 1 &&
+            (event.key === "Enter" || event.key === "," || event.key === ";") &&
+            this.inputValue.trim().length > 0 &&
             this.chips.length < this.limit
-        ) { 
+        ) {
             const chip = sanitizeInput(this.inputValue.trim());
             if (!this.chips.includes(chip)) {
                 this.chips.push(chip);
                 this.onChange(this.chips);
             }
-            this.inputValue = '';
+            this.inputValue = "";
             event.preventDefault();
+            this.chipsInput.nativeElement.focus();
         }
 
-        if (event.key === 'Backspace' && this.inputValue === '') {
+        if (event.key === "Backspace" && this.inputValue === "") {
             this.chips.pop();
+        }
+    }
+
+    public addChipWithButton(): void {
+        const sanitizeInput = (str: string): string => {
+            const regex = /[^a-zA-Z0-9\s]/g;
+            return str.replace(regex, "");
+        };
+
+        if (this.inputValue.trim().length > 0 && this.chips.length < this.limit) {
+            const chip = sanitizeInput(this.inputValue.trim());
+            if (!this.chips.includes(chip)) {
+                this.chips.push(chip);
+                this.onChange(this.chips);
+            }
+            this.inputValue = "";
+            this.chipsInput.nativeElement.focus();
         }
     }
 
     public removeChip(index: number): void {
         this.chips.splice(index, 1);
         this.onChange(this.chips);
+        this.chipsInput.nativeElement.focus();
     }
 }
