ffef1cf47a44d18d8bd20510ee1679ac3c8cf482
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-06-20 13:10:44 +0300

    do not show the map on the topic preview page if location is not selected

--- src/app/shared/constants/app.constants.ts
+++ src/app/shared/constants/app.constants.ts
@@ -13,14 +13,8 @@ const PHONE_LINE_TYPES = {
   unknown: "unknown"
 };
 
-const DEFAULT_USER_LOCATION = {
-  latitude: 35.0078,
-  longitude: -97.0929,
-};
-
 export class AppConstants {
   public static readonly PULSES_PER_PAGE: number = 20;
   public static readonly PHONE_LINE_TYPES = PHONE_LINE_TYPES;
-  public static readonly DEFAULT_USER_LOCATION = DEFAULT_USER_LOCATION;
 }
 
