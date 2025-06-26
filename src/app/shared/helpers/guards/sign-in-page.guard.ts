b17ff4e0ee6670cd3436f3e903bff21f60dd593c
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-06-26 17:25:18 +0300

    avoid call signout when the user is on the signin page

--- src/app/shared/helpers/guards/sign-in-page.guard.ts
+++ src/app/shared/helpers/guards/sign-in-page.guard.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from "@angular/core";
 import { CanActivate } from "@angular/router";
-import { catchError, map, Observable, of } from "rxjs";
+import { catchError, map, Observable, of, switchMap } from "rxjs";
 import { AuthenticationService } from "../../services/api/authentication.service";
 
 @Injectable({
@@ -9,11 +9,17 @@ import { AuthenticationService } from "../../services/api/authentication.service
 export class SignInPageGuard implements CanActivate {
     private readonly authenticationService: AuthenticationService = inject(AuthenticationService);
     canActivate(): Observable<boolean> {
-        return this.authenticationService.logout().pipe(
+        return this.authenticationService.user$.pipe(
+            switchMap((user) => {
+                if (user) {
+                    return this.authenticationService.logout()
+                }
+                return of(user);
+            }),
             catchError(() => {
                 return of(false);
             }),
             map(() => true),
-        );
+        )
     }
 }
