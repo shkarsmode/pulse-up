328fdcc434b337a19dd5be9c73d2c25cc40c46c0
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-08-04 09:53:40 +0300

    display correct message when username contains a reserved word or other error from backend

--- src/app/features/profile/helpers/error-message-builder.ts
+++ src/app/features/profile/helpers/error-message-builder.ts
@@ -1,8 +1,10 @@
 import { AbstractControl } from "@angular/forms";
-import { min } from "rxjs";
 
 export class ErrorMessageBuilder {
-    public static getErrorMessage(control: AbstractControl<any, any>, name: string): string | null {
+    public static getErrorMessage(
+        control: AbstractControl<string, string>,
+        name: string,
+    ): string | null {
         if (!control || !(control.touched || control.dirty) || control.valid) {
             return null;
         }
@@ -15,6 +17,11 @@ export class ErrorMessageBuilder {
             if (control.hasError(errorKey)) {
                 return message;
             }
+        }
+
+        if (control.errors) {
+            const error = Object.values(control.errors)[0];
+            return error || null;
         }
 
         return null;
@@ -31,23 +38,23 @@ export class ErrorMessageBuilder {
             pattern: "Name must contain only letters",
         },
         username: {
-          required: "Username is required",
-          minlength: "Username must be at least 6 characters long",
-          maxlength: "Username cannot exceed 50 characters",
-          pattern: "Username must be alphanumeric and can include one underscore",
-          noLetter: "Username must contain at least one letter",
-          notUnique: "Username is already taken",
+            required: "Username is required",
+            minlength: "Username must be at least 6 characters long",
+            maxlength: "Username cannot exceed 50 characters",
+            pattern: "Username must be alphanumeric and can include one underscore",
+            noLetter: "Username must contain at least one letter",
+            notUnique: "Username is already taken",
         },
         bio: {
-          required: "Bio is required",
-          minlength: "Bio must be at least 3 characters long",
-          maxlength: "Bio cannot exceed 150 characters",
+            required: "Bio is required",
+            minlength: "Bio must be at least 3 characters long",
+            maxlength: "Bio cannot exceed 150 characters",
         },
         profilePicture: {
-          missingName: "Picture must have a name",
-          invalidType: "File must be an image",
-          invalidExtension: "Only .png, .jpg, .jpeg allowed",
-          fileTooLarge: "File must be smaller than 10 MB",
-        }
+            missingName: "Picture must have a name",
+            invalidType: "File must be an image",
+            invalidExtension: "Only .png, .jpg, .jpeg allowed",
+            fileTooLarge: "File must be smaller than 10 MB",
+        },
     };
 }
---
+++ src/app/shared/interfaces/user/username-validation-result.interface.ts
@@ -0,0 +1,4 @@
+export interface UsernameValidationResult {
+    success: boolean;
+    message?: string;
+}
