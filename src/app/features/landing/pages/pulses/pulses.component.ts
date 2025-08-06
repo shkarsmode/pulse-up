4e0a85f07b4dbf62099d1c756aa62919f4fd644e
Eugene Chekerdes <kuharchuk.dmytro1337@gmail.com>
2025-08-06 14:11:37 +0300

    finished PulsesPaginationService

--- src/app/features/landing/pages/pulses/pulses.component.ts
+++ src/app/features/landing/pages/pulses/pulses.component.ts
@@ -1,22 +1,19 @@
-import { Component, DestroyRef, inject, OnInit } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { first, map, Observable } from "rxjs";
 import { InfiniteScrollDirective } from "ngx-infinite-scroll";
-import { IPaginator, ITopic } from "@/app/shared/interfaces";
-import { PulseService } from "@/app/shared/services/api/pulse.service";
+import { ITopic } from "@/app/shared/interfaces";
 import { InputSearchComponent } from "../../ui/input-search/input-search.component";
 import { InfiniteLoaderService } from "../../services/infinite-loader.service";
-import { AppConstants } from "@/app/shared/constants/app.constants";
 import { LoadingIndicatorComponent } from "@/app/shared/components/loading-indicator/loading-indicator.component";
 import { ICategory } from "@/app/shared/interfaces/category.interface";
-import { SpinnerComponent } from "@/app/shared/components/ui-kit/spinner/spinner.component";
 import { TrendingTopicsListItemComponent } from "../../ui/trending-topics-list-item/trending-topics-list-item.component";
 import { VotesService } from "@/app/shared/services/votes/votes.service";
 import { ContainerComponent } from "@/app/shared/components/ui-kit/container/container.component";
 import { CategoryFilterMenuComponent } from "@/app/shared/components/category-filter-menu/category-filter-menu.component";
 import { CategoryFilterSelectionComponent } from "@/app/shared/components/category-filter-menu/category-filter-selection/category-filter-selection.component";
 import { CategoryFilterService } from "@/app/shared/components/category-filter-menu/category-filter.service";
+import { PulsesPaginationService } from "../../services/pulses-pagination.service";
 
 @Component({
     selector: "app-pulses",
@@ -29,27 +26,24 @@ import { CategoryFilterService } from "@/app/shared/components/category-filter-m
         InfiniteScrollDirective,
         LoadingIndicatorComponent,
         InputSearchComponent,
-        SpinnerComponent,
         TrendingTopicsListItemComponent,
         ContainerComponent,
         CategoryFilterMenuComponent,
         CategoryFilterSelectionComponent,
     ],
-    providers: [InfiniteLoaderService],
+    providers: [InfiniteLoaderService, PulsesPaginationService],
 })
-export class PulsesComponent implements OnInit {
-    private readonly destroyRef = inject(DestroyRef);
-    private readonly pulseService = inject(PulseService);
-    private readonly infiniteLoaderService = inject(InfiniteLoaderService);
+export class PulsesComponent {
     private readonly votesService = inject(VotesService);
     private readonly categoryFilterService = inject(CategoryFilterService);
+    private readonly pulsesPaginationService = inject(PulsesPaginationService);
 
     public pulses: ITopic[] = [];
-    public isLoading = true;
-    public loading$: Observable<boolean>;
-    public paginator$: Observable<IPaginator<ITopic>>;
     public searchInFocus = false;
     public votes$ = this.votesService.votesByTopicId$;
+    public topics$ = this.pulsesPaginationService.topics$;
+    public isInitialLoading$ = this.pulsesPaginationService.initialLoading$;
+    public isLoadingMore$ = this.pulsesPaginationService.paginationLoading$;
 
     public get selectedCategory(): ICategory | null {
         return this.categoryFilterService.activeCategory === "all"
@@ -57,23 +51,17 @@ export class PulsesComponent implements OnInit {
             : this.categoryFilterService.activeCategory;
     }
 
-    public ngOnInit(): void {
-        this.getTrendingPulses();
+    public loadMore() {
+        this.pulsesPaginationService.loadMore();
     }
 
-    public loadMore = this.infiniteLoaderService.loadMore.bind(this.infiniteLoaderService);
-
     public onSearchValueChange(searchValue: string): void {
-        this.getTrendingPulses({ keyword: searchValue });
+        this.pulsesPaginationService.setSearchText(searchValue);
     }
 
     public onCategorySelected(category: ICategory | null): void {
         this.categoryFilterService.activeCategory = category || "all";
-        if (!category) {
-            this.getTrendingPulses();
-        } else {
-            this.getTrendingPulses({ category: category.name });
-        }
+        this.pulsesPaginationService.setCategoryFilter(category);
     }
 
     public onSearchFocus(): void {
@@ -82,35 +70,5 @@ export class PulsesComponent implements OnInit {
 
     public onSearchBlur(): void {
         this.searchInFocus = false;
-    }
-
-    private getTrendingPulses({
-        keyword,
-        category,
-    }: { keyword?: string; category?: string } = {}): void {
-        const take = AppConstants.PULSES_PER_PAGE;
-        this.infiniteLoaderService.init({
-            load: (page) =>
-                this.pulseService
-                    .get({
-                        keyword,
-                        category,
-                        take,
-                        skip: take * (page - 1),
-                    })
-                    .pipe(
-                        first(),
-                        map(
-                            (pulses) =>
-                                ({
-                                    page,
-                                    items: pulses,
-                                    hasMorePages: pulses.length !== 0 && pulses.length === take,
-                                }) as IPaginator<ITopic>,
-                        ),
-                    ),
-        });
-        this.loading$ = this.infiniteLoaderService.loading$;
-        this.paginator$ = this.infiniteLoaderService.paginator$;
     }
 }
