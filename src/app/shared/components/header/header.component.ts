f4cfce7f5df6eed015a8c7a6edd3782f64a95bdc
Eugene Chekerdes <kuharchuk.dmytro1337@gmail.com>
2025-08-12 11:06:19 +0300

    open online store directly on the mobile view

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,51 +1,65 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { SvgIconComponent } from 'angular-svg-icon';
-import { AppRoutes } from '../../enums/app-routes.enum';
-import { BurgerButtonComponent } from '../ui-kit/buttons/burger-button/burger-button.component';
-import { PrimaryButtonComponent } from '../ui-kit/buttons/primary-button/primary-button.component';
+import { CommonModule } from "@angular/common";
+import { Component, inject, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { SvgIconComponent } from "angular-svg-icon";
+import { MatIcon } from "@angular/material/icon";
+import { map } from "rxjs";
+import { AppRoutes } from "../../enums/app-routes.enum";
+import { BurgerButtonComponent } from "../ui-kit/buttons/burger-button/burger-button.component";
+
+import { version } from "../../../../assets/data/version";
+import { OpenGetAppPopupDirective } from "../popups/get-app-popup/open-get-app-popup.directive";
+import { AuthenticationService } from "../../services/api/authentication.service";
+import { PlatformService } from "../../services/core/platform.service";
+import { SettingsService } from "../../services/api/settings.service";
+import { DownloadAppButtonComponent } from "./download-app-button/download-app-button.component";
 import { SecondaryButtonComponent } from "../ui-kit/buttons/secondary-button/secondary-button.component";
 
-import { version } from '../../../../assets/data/version';
-import { OpenGetAppPopupDirective } from '../popups/get-app-popup/open-get-app-popup.directive';
-import { AuthenticationService } from '../../services/api/authentication.service';
-import { MatIcon } from '@angular/material/icon';
-
 @Component({
-    selector: 'app-header',
+    selector: "app-header",
     standalone: true,
     imports: [
         CommonModule,
         RouterModule,
         MatIcon,
-        PrimaryButtonComponent,
         SvgIconComponent,
         BurgerButtonComponent,
         FormsModule,
-        SecondaryButtonComponent,
         OpenGetAppPopupDirective,
+        DownloadAppButtonComponent,
+        SecondaryButtonComponent,
     ],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.scss',
+    templateUrl: "./header.component.html",
+    styleUrl: "./header.component.scss",
 })
 export class HeaderComponent implements OnInit {
-    private readonly authService = inject(AuthenticationService);
+    private platformService = inject(PlatformService);
+    private settingsService = inject(SettingsService);
+    private authService = inject(AuthenticationService);
 
     public isMobileDropdown = false;
     public AppRoutes = AppRoutes;
     public version: { major: number; minor: number; patch: number };
-    public isToShowVersionOfApp = !!localStorage.getItem('version');
+    public isToShowVersionOfApp = !!localStorage.getItem("version");
+    public onlineStoreLink$ = this.settingsService.settings$.pipe(
+        map((settings) => {
+            if (this.platformService.value == "iOS") {
+                return settings.appStoreUrl;
+            } else {
+                return settings.googlePlayUrl;
+            }
+        }),
+    );
 
     ngOnInit(): void {
         //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
         //Add 'implements OnInit' to the class.
         this.getCurrentVersionOfApplication();
-    }  
+    }
 
     public get isAuthenticated() {
-        return !!this.authService.userTokenValue
+        return !!this.authService.userTokenValue;
     }
 
     public toggleDropdown(): void {
@@ -58,18 +72,18 @@ export class HeaderComponent implements OnInit {
 
     private disableDocumentScroll(): void {
         setTimeout(() => window.scrollTo(0, 0), 100);
-        document.body.classList.add('no-scroll');
+        document.body.classList.add("no-scroll");
     }
 
     private enableDocumentScroll(): void {
-        document.body.classList.remove('no-scroll');
+        document.body.classList.remove("no-scroll");
     }
 
     public deligateCloseDropdown(event: Event) {
         const targetElement = event.target as HTMLElement;
 
         // Check if the click occurred on an anchor tag (logo or menu links)
-        if (targetElement.tagName === 'A') {
+        if (targetElement.tagName === "A") {
             this.isMobileDropdown = false;
             this.enableDocumentScroll();
         }
@@ -90,7 +104,7 @@ export class HeaderComponent implements OnInit {
     public scrollToTop(): void {
         window.scrollTo({
             top: 0,
-            behavior: 'smooth',
+            behavior: "smooth",
         });
     }
 
