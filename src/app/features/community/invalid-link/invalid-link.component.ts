dba5467430add2966bff7c7f95b04e8ee34c1e26
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-08-15 15:26:59 +0300

    add PrimaryButtonComponent to imports in InvalidLinkComponent

--- src/app/features/community/invalid-link/invalid-link.component.ts
+++ src/app/features/community/invalid-link/invalid-link.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppRoutes } from '../../../shared/enums/app-routes.enum';
 import { AngularSvgIconModule } from "angular-svg-icon";
+import { PrimaryButtonComponent } from '@/app/shared/components/ui-kit/buttons/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-invalid-link',
   templateUrl: './invalid-link.component.html',
   styleUrl: './invalid-link.component.scss',
   standalone: true,
-  imports: [AngularSvgIconModule, RouterModule],
+  imports: [AngularSvgIconModule, RouterModule, PrimaryButtonComponent],
 })
 export class InvalidLinkComponent {
   public routes = AppRoutes.Landing;
