7c1c0f99831bd831f1eed7ccd32efee90fa90d7f
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-09-03 09:54:05 +0300

    save geolocation to local storage for geolocation caching

--- src/app/shared/services/core/geolocation-cache.service.ts
+++ src/app/shared/services/core/geolocation-cache.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { IGeolocation } from "../../interfaces";
+import { LOCAL_STORAGE_KEYS, LocalStorageService } from "./local-storage.service";
 
 @Injectable({
     providedIn: "root",
@@ -8,10 +9,10 @@ export class GeolocationCacheService {
     private geolocationCache: IGeolocation | null = null;
     private timestamp: number | null = null;
     private readonly expiryTime = 1000 * 60 * 60; // 1 hour
+    private readonly storageKey = LOCAL_STORAGE_KEYS.geolocationCache;
 
-    public save(geolocation: IGeolocation) {
-        this.geolocationCache = geolocation;
-        this.timestamp = Date.now();
+    constructor() {
+        this.syncWithStorage();
     }
 
     public get(): IGeolocation | null {
@@ -21,12 +22,43 @@ export class GeolocationCacheService {
         return this.geolocationCache;
     }
 
+    public save(geolocation: IGeolocation) {
+        this.geolocationCache = geolocation;
+        this.timestamp = Date.now();
+        this.saveToStorage();
+    }
+
     public clear() {
         this.geolocationCache = null;
         this.timestamp = null;
+        this.removeFromStorage();
     }
 
     private isExpired(): boolean {
         return !!this.timestamp && Date.now() - this.timestamp > this.expiryTime;
+    }
+
+    private syncWithStorage() {
+        const storedData = LocalStorageService.get<{
+            geolocation: IGeolocation;
+            timestamp: number;
+        }>(this.storageKey);
+        if (storedData) {
+            this.geolocationCache = storedData.geolocation;
+            this.timestamp = storedData.timestamp;
+        }
+    }
+
+    private saveToStorage() {
+        if (this.geolocationCache && this.timestamp) {
+            LocalStorageService.set(this.storageKey, {
+                geolocation: this.geolocationCache,
+                timestamp: this.timestamp,
+            });
+        }
+    }
+
+    private removeFromStorage() {
+        LocalStorageService.remove(this.storageKey);
     }
 }
