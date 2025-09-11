c9e58123031551262ec8926fa0af0ed010347bd5
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-09-11 13:00:38 +0300

    fix isUtcNextDay function

--- src/app/features/landing/helpers/isUtcNextDay.ts
+++ src/app/features/landing/helpers/isUtcNextDay.ts
@@ -3,10 +3,8 @@ import utc from "dayjs/plugin/utc";
 
 dayjs.extend(utc);
 
-export function isUtcNextDay(): boolean {
-    const localDate = dayjs().get("date");
+export function isUtcNextDay(date: Date): boolean {
+    const localDate = dayjs(date).get("date");
     const utcDate = dayjs().utc().get("date");
-    console.log({ localDay: localDate, utcDay: utcDate });
-
     return utcDate - localDate === 1;
 }
