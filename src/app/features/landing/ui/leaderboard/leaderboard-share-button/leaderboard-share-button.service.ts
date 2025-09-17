5506201df027ac13ca2e8ccff27556587df0020c
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-09-17 10:32:34 +0300

    Refactor leaderboard share button logic and update share URL generation

--- src/app/features/landing/ui/leaderboard/leaderboard-share-button/leaderboard-share-button.service.ts
+++ src/app/features/landing/ui/leaderboard/leaderboard-share-button/leaderboard-share-button.service.ts
@@ -1,13 +1,28 @@
 import { inject, Injectable } from "@angular/core";
-import { SettingsService } from "@/app/shared/services/api/settings.service";
 import { toSignal } from "@angular/core/rxjs-interop";
 import { map } from "rxjs";
+import { SettingsService } from "@/app/shared/services/api/settings.service";
 
 @Injectable({
     providedIn: "root",
 })
 export class LeaderboardShareButtonService {
     private settingsService = inject(SettingsService);
+    private shareLeaderboardBaseUrl = toSignal(
+        this.settingsService.settings$.pipe(map((settings) => settings.shareLeaderboardBaseUrl)),
+        { initialValue: "" },
+    );
+    public getShareUrl() {
+        const params = new URLSearchParams(window.location.search);
+        const date = params.get("date");
+        const timeframe = params.get("timeframe");
+        const locationName = params.get("locationName");
+        const newParams = new URLSearchParams();
+        if (date) newParams.set("d", date);
+        if (timeframe) newParams.set("t", timeframe);
+        if (locationName) newParams.set("n", locationName);
+        return `${this.shareLeaderboardBaseUrl()}?${newParams.toString()}`;
+    }
     public shareUrl = toSignal(
         this.settingsService.settings$.pipe(
             map((settings) => settings.shareLeaderboardBaseUrl),
