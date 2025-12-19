7281be80bf033bcc2ffcdab09145af913d7a5a8e
Shkarupa Daniil <zshkarrr@gmail.com>
2025-12-19 14:48:29 +0200

    Add site stats and formating for total votes

--- src/app/features/landing/pages/main/components/top-pulses/top-pulses.component.ts
+++ src/app/features/landing/pages/main/components/top-pulses/top-pulses.component.ts
@@ -1,12 +1,7 @@
-import { Component, inject } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
-import { AppRoutes } from "@/app/shared/enums/app-routes.enum";
-import { Colors } from "@/app/shared/enums/colors.enum";
-import { SecondaryButtonComponent } from "@/app/shared/components/ui-kit/buttons/secondary-button/secondary-button.component";
+import { TopPulsesService } from "@/app/features/landing/pages/main/components/top-pulses/top-pulses.service";
 import {
-    SmallPulseComponent,
     SmallPulseBadgeComponent,
+    SmallPulseComponent,
     SmallPulseHeaderComponent,
     SmallPulseIconComponent,
     SmallPulseStatsComponent,
@@ -14,9 +9,17 @@ import {
     SmallPulseSubtitleComponent,
     SmallPulseTitleComponent,
 } from "@/app/shared/components/pulses/small-pulse";
-import { TopPulsesService } from "@/app/features/landing/pages/main/components/top-pulses/top-pulses.service";
-import { FormatNumberPipe } from "@/app/shared/pipes/format-number.pipe";
+import { SecondaryButtonComponent } from "@/app/shared/components/ui-kit/buttons/secondary-button/secondary-button.component";
 import { SkeletonComponent } from "@/app/shared/components/ui-kit/skeleton/skeleton.component";
+import { AppRoutes } from "@/app/shared/enums/app-routes.enum";
+import { Colors } from "@/app/shared/enums/colors.enum";
+import { ISiteStats } from '@/app/shared/interfaces/site-stats.interface';
+import { FormatNumberPipe } from "@/app/shared/pipes/format-number.pipe";
+import { PulseService } from '@/app/shared/services/api/pulse.service';
+import { CommonModule } from "@angular/common";
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { RouterModule } from "@angular/router";
+import { first } from 'rxjs';
 
 @Component({
     selector: "app-top-pulses",
@@ -39,7 +42,7 @@ import { SkeletonComponent } from "@/app/shared/components/ui-kit/skeleton/skele
         SkeletonComponent,
     ],
 })
-export class TopPulsesComponent {
+export class TopPulsesComponent implements OnInit {
     private topPulsesService = inject(TopPulsesService);
 
     public topics = this.topPulsesService.topics;
@@ -47,4 +50,18 @@ export class TopPulsesComponent {
     public isError = this.topPulsesService.isError;
     public AppRoutes = AppRoutes;
     public buttonColor = Colors.BLACK;
+
+    public stats: WritableSignal<ISiteStats> = signal({ 
+        activeTopics: 0, 
+        totalUsers: 0, 
+        totalVotes: 0, 
+        lastDayVotes: 0 
+    });
+
+    private pulseService = inject(PulseService);
+    ngOnInit(): void {
+        this.pulseService.getSiteStats().pipe(first()).subscribe((stats) => {
+            this.stats.set(stats);
+        })
+    }
 }
