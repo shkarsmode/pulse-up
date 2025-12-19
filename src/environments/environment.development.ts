ec1f3fd66473e52ec59f458aff2a7dacacdf1b05
shkarsmode <zshkarrr@gmail.com>
2025-12-19 23:46:22 +0200

    Update env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://app-pulseapi-dev-fvdbe3fwhsghczfn.eastus2-01.azurewebsites.net',
+    production: true,
+    apiUrl: 'https://app-pulseapi-prod-bxdye7b9b2fdc0b5.eastus2-01.azurewebsites.net',
     geocodingApiUrl: 'https://api.mapbox.com/geocoding/v5',
     mapboxToken:
         'pk.eyJ1IjoibmF2ZWVuZ29wdWxzZSIsImEiOiJjbTFxb2YyYzIwMjc3MmtvbXRob20yNGY2In0.7Eyobg6zrbOwPXMmW6BAMA',
@@ -9,13 +9,12 @@ export const environment = {
     mapWithBackgroundStyleUrl:
         'mapbox://styles/naveengopulse/cmafp4osk00vy01r40rhx06yk?optimize=true',
     firebaseConfig: {
-        apiKey: "AIzaSyD2qwEcQb9H_R5rohjC78H5CQGdaIvtaOI",
-        authDomain: "pulseapp-d9cea.firebaseapp.com",
-        projectId: "pulseapp-d9cea",
-        storageBucket: "pulseapp-d9cea.firebasestorage.app",
-        messagingSenderId: "301057533103",
-        appId: "1:301057533103:web:388935743e165e31bf205a",
-        measurementId: "G-W6FV6E25JL"
+        apiKey: 'AIzaSyD2qwEcQb9H_R5rohjC78H5CQGdaIvtaOI',
+        authDomain: 'pulseapp-d9cea.firebaseapp.com',
+        projectId: 'pulseapp-d9cea',
+        storageBucket: 'pulseapp-d9cea.appspot.com',
+        messagingSenderId: '301057533103',
+        appId: '1:301057533103:web:388935743e165e31bf205a',
     },
     ipInfoApiToken: "7781b0329f5236",
 };
